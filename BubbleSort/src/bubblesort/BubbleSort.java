b0da73d2075da1ae6342ed136b03f206d41911f8
Laxman-Nath <nathlaxman242@gmail.com>
2024-02-10 11:08:50 +0545

    Initial commit

---
+++ BubbleSort/src/bubblesort/BubbleSort.java
@@ -0,0 +1,56 @@
+/*Bubble sort code in java.Here we are generating random numbers between 1 to 100 
+and storing them into the file.Then we sort those numbers using bubble sort and again
+store them into the another file.Also here we are calculating the time for sorting.
+*/
+package bubblesort;
+import java.util.*;
+import java.time.LocalTime;
+import java.io.FileWriter;
+import java.time.temporal.ChronoUnit;
+
+public class BubbleSort {
+
+    
+    public static void main(String[] args) throws Exception {
+        FileWriter Writer=new FileWriter("D:/BubblesortUnsorted.txt");
+        FileWriter Writer2=new FileWriter("D:/BubblesortSorted.txt");
+        int A[]=new int[100000];
+        int temp,n=100000;
+        Random r=new Random();
+        for(int i=0;i<100000;i++){
+        A[i]=1+r.nextInt(100); // Generating random numbers 
+        }
+       
+        for(int i=0;i<100000;i++){
+             
+            Writer.write(A[i]+","); // Writing unsorted numbers into the file
+        }
+        
+        LocalTime starttime=LocalTime.now();
+        System.out.println("Sorting started at:"+starttime);
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<n-i-1;j++){
+                if(A[j]>A[j+1]){
+                    temp=A[j];
+                    A[j]=A[j+1];
+                    A[j+1]=temp;
+                }
+            }
+        }
+        LocalTime endtime=LocalTime.now();
+        System.out.println("Sorting ended at:"+endtime);
+        long hours=ChronoUnit.HOURS.between(starttime,endtime);
+        long minutes=ChronoUnit.MINUTES.between(starttime,endtime)%60;
+        long seconds=ChronoUnit.SECONDS.between(starttime, endtime)%60;
+        System.out.println("The total time taken for sorting is:"+hours+" hours "+minutes
+        +" minutes "+seconds+" seconds ");
+        for(int i=0;i<100000;i++){
+             
+            Writer2.write(A[i]+",");// Writting sorted numbers into the file
+        }
+       
+        
+        
+    }
+    
+}
